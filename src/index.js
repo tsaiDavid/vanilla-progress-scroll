c973951d46ec9233b30e1e963098b74a6ebce496
David Tsai <tsaiDavid@users.noreply.github.com>
2018-06-27 09:51:49 -0700

    Implemented first pass with styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+const pidEl = document.getElementById("progress-indicator");
+
 document.onscroll = function() {
   const windowHeight = window.innerHeight;
   const contentScrolled = window.pageYOffset;
   const bodyHeight = document.body.offsetHeight;
 
   if (bodyHeight - contentScrolled <= windowHeight) {
-    console.log("100%");
+    pidEl.style.width = "100%";
   } else {
     const num = contentScrolled / (bodyHeight - windowHeight) * 100;
     const numAsString = `${parseInt(num)}%`;
-    console.log(numAsString);
+    pidEl.style.width = numAsString;
   }
 };
